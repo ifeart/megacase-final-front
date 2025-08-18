30ecdec4fed7a37f90e9f820f32882d904aeec09
ifeart mc <905ivan1@gmail.com>
2025-08-18 09:37:32 +0400

    change: InternalPage and Route logic

--- src/pages/InternalPage.tsx
+++ src/pages/InternalPage.tsx
@@ -1,3 +1,15 @@
-export default function InternalPage() {
-  return <div>InternalPage</div>;
+import type { ReactNode } from "react";
+import InternalHeader from "../components/InternalHeader";
+
+type InternalPageProps = {
+  children: ReactNode;
+};
+
+export default function InternalPage({ children }: InternalPageProps) {
+  return (
+    <>
+      <InternalHeader />
+      <main className="pt-16 h-[calc(100vh-64px)]">{children}</main>
+    </>
+  );
 }
--- src/pages/SetupRoute.tsx
+++ src/pages/SetupRoute.tsx
@@ -1,15 +1,12 @@
 import { useNavigate } from "react-router-dom";
-import InternalHeader from "../components/InternalHeader";
 import { SpaceSetup } from "../features/admin/SpaceSetup";
+import InternalPage from "./InternalPage";
 
 export default function SetupRoute() {
   const navigate = useNavigate();
   return (
-    <>
-      <InternalHeader />
-      <main className="pt-16 h-[calc(100vh-64px)]">
-        <SpaceSetup onDone={(sid) => navigate(`/editor/${sid}`)} />
-      </main>
-    </>
+    <InternalPage>
+      <SpaceSetup onDone={(sid) => navigate(`/editor/${sid}`)} />
+    </InternalPage>
   );
 }
